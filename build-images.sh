04ecec9f5a4e1377998d5c405868effd746c692d
Laurent Prevost <laurent.prevost@heig-vd.ch>
2016-08-17 15:01:49 +0200

    Removed sudo

--- build-images.sh
+++ build-images.sh
@@ -3,9 +3,9 @@
 # Build the haproxy image
 echo "************************  build haproxy image  ************************"
 cd /vagrant/ha
-sudo docker build -t softengheigvd/ha .
+docker build -t softengheigvd/ha .
 
 # Build the webapp image
 echo "************************  build webapp image  ************************"
 cd /vagrant/webapp
-sudo docker build -t softengheigvd/webapp .
+docker build -t softengheigvd/webapp .
--- start-containers.sh
+++ start-containers.sh
@@ -2,12 +2,12 @@
 
 # Run two webapps
 echo "************************  run webapps  ************************"
-sudo docker rm -f s1 2>/dev/null || true
-sudo docker rm -f s2 2>/dev/null || true
-sudo docker run -d --name s1 softengheigvd/webapp
-sudo docker run -d --name s2 softengheigvd/webapp
+docker rm -f s1 2>/dev/null || true
+docker rm -f s2 2>/dev/null || true
+docker run -d --name s1 softengheigvd/webapp
+docker run -d --name s2 softengheigvd/webapp
 
 # Run load balancer
 echo "************************  run haproxy  ************************"
-sudo docker rm -f ha 2>/dev/null || true
-sudo docker run -d  -p 80:80 -p 1936:1936 -p 9999:9999 --link s1 --link s2 --name ha softengheigvd/ha
+docker rm -f ha 2>/dev/null || true
+docker run -d  -p 80:80 -p 1936:1936 -p 9999:9999 --link s1 --link s2 --name ha softengheigvd/ha
