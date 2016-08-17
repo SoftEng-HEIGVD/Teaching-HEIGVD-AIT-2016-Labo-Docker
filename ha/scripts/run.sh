e0894d48bcdc03dc9b0de1e9dcf68b0f4e844fd0
Laurent Prevost <laurent.prevost@heig-vd.ch>
2016-08-17 14:22:18 +0200

    Added comments

--- ha/scripts/run.sh
+++ ha/scripts/run.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 rsyslogd -c5 2>/dev/null
 
+# TODO: [CFG] Remove the following two lines
 sed -i 's/<s1>/$S1_PORT_3000_TCP_ADDR/g' /usr/local/etc/haproxy/haproxy.cfg
 sed -i 's/<s2>/$S2_PORT_3000_TCP_ADDR/g' /usr/local/etc/haproxy/haproxy.cfg
 
+# TODO: [CFG] Replace this command
 haproxy -f /usr/local/etc/haproxy/haproxy.cfg -p /var/run/haproxy.pid
